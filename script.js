390bd13b014ff383890697516af1755d4e84d81c
yassun32 <yassinblailastor15@gmail.com>
2026-09-01 10:25:31 +0100

    add prompt sync script

---
+++ script.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')();
+const name = prompt('What is your name? ');const color = prompt('What is your favorite color? ');
+
+console.log(`Hello ${name}! Your favorite color is ${color}.`);
