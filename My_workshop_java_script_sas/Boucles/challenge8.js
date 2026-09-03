5dc78ea2d3358c38a515d8c018567646046857f8
yassun32 <yassinblailastor15@gmail.com>
2026-09-03 16:40:22 +0100

    add challenge8.js

--- My_workshop_java_script_sas/Boucles/challenge8.js
+++ My_workshop_java_script_sas/Boucles/challenge8.js
@@ -1,1 +1,7 @@
 const prompt = require("prompt-sync");
+const n = prompt = ("enter le nombre n : ");
+if (n == 0 || n == 1)
+    console.log(n);
+else if (n >= 2){
+                                                                                                                                                                              
+}
